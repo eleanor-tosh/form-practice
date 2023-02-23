00ed59c767ae287eab734a6094181b1b226105d0
Eleanor Tosh <eleanortosh@outlook.com>
2023-02-23 19:21:23 +1300

    pink feedback form replicated

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -3,8 +3,30 @@ import '../styles/feedback.css'
 
 function Feedback() {
   return (
-    <div>
-      <img src="/images/basic-feedback.png" alt="feedback form to replicate" />
+    <div className="container">
+      <div>
+        <img
+          src="/images/basic-feedback.png"
+          alt="feedback form to replicate"
+        />
+      </div>
+      {/* Form */}
+      <div>
+        <div className="form-pg">
+          <form>
+            <h2>FEEDBACK FORM</h2>
+            <input type="string" placeholder="Name"></input>
+            <input type="string" placeholder="Email"></input>
+            <input type="string" placeholder="Subject"></input>
+            <input
+              className="textarea"
+              type="textarea"
+              placeholder="Message"
+            ></input>
+            <button className="btn-grad">SUBMIT</button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
