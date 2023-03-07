56b7a5789ac142b3cef2d806b810af5ece60158c
Eleanor Tosh <eleanortosh@outlook.com>
2023-03-07 21:15:05 +1300

    flex form

--- src/pages/Flex.jsx
+++ src/pages/Flex.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 import styles from '../styles/flex.module.css'
 
 function Flex() {
-  return <div></div>
+  return (
+    <div>
+      <form className={styles.formflex}>
+        <div className={styles.signin}>Sign In</div>
+        <div className={styles.inputs}>
+          <div className={styles.flexer}>
+            <label>Email</label>
+            <input placeholder="email" />
+          </div>
+          <div className={styles.flexer}>
+            <label>Password</label>
+            <input placeholder="password" />
+          </div>
+        </div>
+        <div className={styles.buttondiv}>
+          <button className={styles.button}>Submit</button>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default Flex
